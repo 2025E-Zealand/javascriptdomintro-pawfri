6bc0568c4f347b321f05ffb5b598106c1905643a
Patrick <pfriis@outlook.com>
2025-09-30 11:47:54 +0200

    Add ElementsByTagName & ElementsByClassName

--- js/list.js
+++ js/list.js
@@ -1,7 +1,18 @@
 
 console.log("Skriv dit Javascript her")
 
+// TASK 1
+
+// Get Element By Id Metode
 let myElement = document.getElementById("one"); 
 console.log(myElement)
+
+// Get Element By Tag Name
+let myList = document.getElementsByTagName("li")
+console.log(myList)
+
+// Get Element By Class Name
+let myClass = document.getElementsByClassName("hot")
+console.log(myClass)
 
 console.log("slut på JavaScript")
