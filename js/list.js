a6ac54c3077d4ccf2cd83d527a16f838908bd7fa
Patrick <pfriis@outlook.com>
2025-10-01 10:00:51 +0200

    Task 2 Find specific items and change attributes

--- js/list.js
+++ js/list.js
@@ -15,4 +15,21 @@ console.log(myList)
 let myClass = document.getElementsByClassName("hot")
 console.log(myClass)
 
+// TASK 2
+
+// Find a specific item, change the “class” attribute on this item
+let changeAttribute = document.getElementById("one");
+changeAttribute.setAttribute("class","cool");
+
+// Find a specific item, add the attribute hidden= true” to hide the element I the browser
+// Hidden id 'two': pine nuts
+let hideAttribute = document.getElementById("two");
+hideAttribute.hidden = true;
+
+// Find a specific nodes, e.g. all items with class=” unhealty try to change the class attribute
+let UnhealthyItem = Array.from(document.getElementsByClassName("unhealthy"));
+
+UnhealthyItem[0].setAttribute("class", "hot");
+UnhealthyItem[1].setAttribute("class", "hot");
+
 console.log("slut på JavaScript")
