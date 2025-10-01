c13ef6d0abcef653949908a03b4ce46f02d8ae30
Patrick <pfriis@outlook.com>
2025-10-01 10:34:29 +0200

    Task 3 Find parent, firstChild and lastChild

--- js/list.js
+++ js/list.js
@@ -2,10 +2,11 @@
 console.log("Skriv dit Javascript her")
 
 // TASK 1
+console.log("------ TASK 1 ------");
 
 // Get Element By Id Metode
 let myElement = document.getElementById("one"); 
-console.log(myElement)
+console.log(myElement.textContent)
 
 // Get Element By Tag Name
 let myList = document.getElementsByTagName("li")
@@ -16,6 +17,8 @@ let myClass = document.getElementsByClassName("hot")
 console.log(myClass)
 
 // TASK 2
+console.log("------ TASK 2 ------");
+console.log("Nothing to log, only visual changes to page");
 
 // Find a specific item, change the “class” attribute on this item
 let changeAttribute = document.getElementById("one");
@@ -33,10 +36,24 @@ UnhealthyItem[0].setAttribute("class", "hot");
 UnhealthyItem[1].setAttribute("class", "hot");
 
 // TASK 3
+console.log("------ TASK 3 ------");
 
+// Find an item in the list. Use the ParentNode to get the parent node
+let honeyItem = document.getElementById("three");
+let honeyParent = honeyItem.parentNode;
+console.log(honeyParent);
 
+// Find the FirstChild and LastChild in the list, try to change the class attribute to change the 
+// style so you can see that you have found the right node.
+let honeyParentFirstChild = honeyParent.firstElementChild;
+let honeyParentLastChild = honeyParent.lastElementChild;
+console.log(honeyParentFirstChild)
+console.log(honeyParentLastChild)
 
+honeyParentFirstChild.setAttribute("class", "hot")
+honeyParentLastChild.setAttribute("class", "cool")
 
-
+// TASK 4
+console.log("------ TASK 4 ------");
 
 console.log("slut på JavaScript")
